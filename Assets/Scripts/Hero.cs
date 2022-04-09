e68b47ec47589e4e4ae7f79c0026188395aa47df
Pavel Gordienko <guzmo6@rambler.ru>
2022-04-09 22:10:48 +0300

    physics

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpSpeed;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _groundCheckRadius;
+    [SerializeField] private Vector3 _groundCheckPoisitionDelta;
+    
+    private Rigidbody2D _rigidbody;
+    private Vector2 _direction;
+    
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector2(_direction.x * _speed,_rigidbody.velocity.y);
+        
+        // прыжок
+        bool isJumping = _direction.y > 0;
+        if (isJumping)
+        {
+            if (isGraunded())
+            {
+                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            } 
+        }
+        //для высоты прыжка
+        // если мы не прыгаем (на нажимаем), но летим вверх, то замедляемся
+        else if (_rigidbody.velocity.y > 0 )
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+        }
+
+        
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        _direction = direction;
+    }
+    
+    //пересекается ли объект с землей
+    private bool isGraunded()
+    {
+        //луч
+        //var hit = Physics2D.Raycast(transform.position, Vector2.down,1,_groundLayer);
+        //сфера
+         var hit = Physics2D.CircleCast(transform.position +_groundCheckPoisitionDelta, _groundCheckRadius,Vector2.down,0, _groundLayer);
+         return hit.collider != null;
+        
+    }
+    
+    //отслеживание
+    private void OnDrawGizmos()
+    {
+        //Debug.DrawRay(transform.position,Vector3.down, isGraunded() ? Color.green : Color.red);
+        Gizmos.color = isGraunded() ? Color.green : Color.red;
+        Gizmos.DrawSphere(transform.position + _groundCheckPoisitionDelta, _groundCheckRadius);
+    }
+    
+    
+}
---
+++ Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+{
+    // пользовательский ввод
+    [SerializeField] private Hero _hero;
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        var direction = context.ReadValue<Vector2>();
+        _hero.SetDirection(direction);
+    }
+}
+ 
