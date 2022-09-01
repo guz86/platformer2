30bad998441b20be01df93987cd91736cf68f7da
Pavel Gordienko <guzmo6@rambler.ru>
2022-09-01 21:29:51 +0300

    add MainMenu, Button, CustomEditor

---
+++ Assets/Scripts/UI/Widgets/CustomButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Widgets
+{
+    public class CustomButton : Button
+    {
+        // создали свою кнопку и добавили 2 параметра
+        // и включаем и выключаем их в зависимости от состояния кнопки
+        
+        // в зависимости от состояния кнопки, будем включать один или другой
+        [SerializeField] private GameObject _normal;
+        [SerializeField] private GameObject _pressed;
+
+        // как поймем что кнопка нажата
+        protected override void DoStateTransition(SelectionState state, bool instant)
+        {
+            
+            // чтобы не потерять функционал
+            base.DoStateTransition(state, instant);
+            
+            // в зависимости от состояния
+            _normal.SetActive(state != SelectionState.Pressed);
+            _pressed.SetActive(state == SelectionState.Pressed);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Widgets/Editor/CustomButtonEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+namespace UI.Widgets.Editor
+{
+    // для того чтобы отобразить нужные нам поля через CustomEditor
+    
+    // для компонента CustomButton мы будем его использовать
+    [CustomEditor(typeof(CustomButton), true)]
+    [CanEditMultipleObjects]
+    public class CustomButtonEditor : ButtonEditor
+    {
+        // код который будет работать только в редакторе, для добавления полей
+        public override void OnInspectorGUI()
+        {
+            // добавим два свойства из CustomButton 
+            // отрисует нам нужные property
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_normal"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_pressed"));
+            // применить изменения
+            serializedObject.ApplyModifiedProperties();
+            
+            base.OnInspectorGUI();
+        }
+    }
+}
