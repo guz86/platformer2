3baef22324ea2fe4926d39c377cf967ade22f0ec
Pavel Gordienko <guzmo6@rambler.ru>
2022-09-02 00:56:15 +0300

    add animated UI/Menu add SettingsWindow add Actions for Buttons

---
+++ Assets/Scripts/UI/AnimatedWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+    public class AnimatedWindow : MonoBehaviour
+    {
+        private Animator _animator;
+        private static readonly int Show = Animator.StringToHash("Show");
+        private static readonly int Hide = Animator.StringToHash("Hide");
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _animator.SetTrigger(Show);
+        }
+
+        public void Close()
+        {
+            _animator.SetTrigger(Hide);
+        }
+        
+        // т.к. Hide операция не синхронная выполняется какоето время
+        // уничтожить окно после закрытия, вызов будет из Event в Hide анимации
+        //virtual чтобы можно было переопределять
+        public virtual void OnCloseAnimationComplete()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MainMenu/MainMenuWindow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.MainMenu
+{
+    // обрабатывает клики на кнопки меню
+    
+    public class MainMenuWindow : AnimatedWindow
+    {
+        private Action _closeAction;
+        
+        public void OnShowSettings()
+        {
+            // путь к префабам UI
+            // загружаем наш префаб, находим канвас и в нем создаем объект префаба
+            var window = Resources.Load<GameObject>("UI/SettingsWindow");
+            var canvas = FindObjectOfType<Canvas>();
+            Instantiate(window, canvas.transform);
+        }
+        
+        // сработают (анонимные функции) после проигрывания анимации
+
+        public void OnStartGame()
+        {// помещаем код анонимной функции загрузка нового уровня
+            _closeAction = () => { SceneManager.LoadScene("Level1"); };
+            Close();
+        }
+
+        public void OnExit()
+        {
+            // помещаем код анонимной функции закрытия приложения, или остановки
+            _closeAction = () =>
+            {
+                
+                Application.Quit();
+            
+                //для редактора остановим проигрывание
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
+                
+            };
+            Close();
+        }
+
+        public override void OnCloseAnimationComplete()
+        {
+            // вызываем по нажатию на кнопку действие и убираем окно
+            _closeAction?.Invoke();
+            base.OnCloseAnimationComplete();
+        }
+    }
+}
